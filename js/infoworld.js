cae101a285297f414cd85bd5138e6ca2c47d4243
Eric <Ericrius1@gmail.com>
2014-10-09 13:07:47 -0600

    got object in

--- js/infoworld.js
+++ js/infoworld.js
@@ -1,6 +1,3 @@
-
-
-
 var parameters = (function() {
   var parameters = {};
   var parts = window.location.search.substr(1).split('&');
@@ -11,150 +8,192 @@ var parameters = (function() {
   return parameters;
 })();
 
+InfoWorld = function(param) {
+  Vizi.Application.call(this, param);
 
-G.loader = new Loader();
-var controls, effect;
+  G.loader = new Loader();
+  var controls, effect;
 
-var controls2, clock = new THREE.Clock();
+  var controls2, clock = new THREE.Clock();
 
-var sky, water;
+  var sky, water;
 
-var cameraPath;
+  var cameraPath;
 
-G.primitives = {}
+  G.primitives = {}
 
-G.shaders = new ShaderLoader('shaders')
-
-
-G.loader.addLoad()
-G.shaders.load('vs-strand', 'strand', 'vertex');
-G.shaders.load('fs-strand', 'strand', 'fragment');
-
-G.shaders.shaderSetLoaded = function() {
-  G.loader.onLoad();
-}
-var c4dLoader = new THREE.C4DLineLoader();
-G.loader.addLoad();
-c4dLoader.load('models/campath-3.txt', function(line) {
-  G.loader.onLoad();
-  cameraPath = line;
-
-});
-
-var objLoader = new THREE.ObjectLoader();
-G.loader.addLoad();
-objLoader.load('models/ID-scene-2.json', function(sceneObj) {
-
-  G.sceneObj = sceneObj;
-  G.loader.onLoad();
-  surface = sceneObj.getObjectByName('surface', true)
-  surface.children[0].material.wireframe = true;
-  surface.children[0].material.transparent = true;
-  surface.children[0].material.opacity = 0.2;
-
-});
-
-G.loader.addLoad();
-$.get('models/sampleData.json', function(data) {
-  G.primitiveData = data;
-  G.loader.onLoad();
-})
-
-G.loader.onStart = function() {
-  this.init();
-  this.animate();
-}.bind(G)
-
-VRClient.ready().then(function() {
-  console.log('kicked off ready');
-});
+  G.shaders = new ShaderLoader('shaders')
 
 
-G.scene = new THREE.Scene();
-G.scene.fog = new THREE.Fog(0x00000, -4000, 80000);
-G.init = function() {
-  // this.createPrimitives();
-  G.renderer = new THREE.WebGLRenderer({
-    antialias: true
-  });
-  G.renderer.autoClear = false;
-  document.body.appendChild(G.renderer.domElement);
+  G.loader.addLoad()
+  G.shaders.load('vs-strand', 'strand', 'vertex');
+  G.shaders.load('fs-strand', 'strand', 'fragment');
 
-  dolly = new THREE.Object3D();
-  dolly.position.set(500, 500, 500);
-  G.scene.add(dolly);
-
-  G.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000000);
-  dolly.add(G.camera);
-
-  if (parameters.mode === 'cardboard') {
-
-    controls = new THREE.DeviceOrientationControls(G.camera);
-    effect = new THREE.StereoEffect(G.renderer);
-
-  } else {
-
-    controls = new THREE.VRControls(G.camera);
-    effect = new THREE.VREffect(G.renderer);
-
+  G.shaders.shaderSetLoaded = function() {
+    G.loader.onLoad();
   }
-
-  effect.setSize(window.innerWidth, window.innerHeight);
-
-  document.body.addEventListener('dblclick', function() {
-
-    effect.setFullScreen(true);
+  var c4dLoader = new THREE.C4DLineLoader();
+  G.loader.addLoad();
+  c4dLoader.load('models/campath-3.txt', function(line) {
+    G.loader.onLoad();
+    cameraPath = line;
 
   });
 
+  var objLoader = new THREE.ObjectLoader();
+  G.loader.addLoad();
+  objLoader.load('models/ID-scene-2.json', function(sceneObj) {
 
+    G.sceneObj = sceneObj;
+    G.loader.onLoad();
+    surface = sceneObj.getObjectByName('surface', true)
+    surface.children[0].material.wireframe = true;
+    surface.children[0].material.transparent = true;
+    surface.children[0].material.opacity = 0.2;
 
-  G.scene.add(G.sceneObj);
+  });
 
-
-
-}
-G.createPrimitives = function() {
-  G.primitives['arc'] = new G.Arc();
-  G.primitives['fresnal'] = new G.Fresnal();
-  _.each(G.primitiveData, function(data, name) {
-    if (G.primitives[name]) {
-      G.primitives[name].init(data.numClones, data.positionData, data.sizeRange);
-    }
+  G.loader.addLoad();
+  $.get('models/sampleData.json', function(data) {
+    G.primitiveData = data;
+    G.loader.onLoad();
   })
+
+  VRClient.ready().then(function() {
+    console.log('kicked off ready');
+  });
+
+  G.loader.onStart = function() {
+    Vizi.Application.instance.init()
+    Vizi.Application.instance.run()
+  }.bind(G)
 }
 
-G.onResize = function() {
+goog.inherits(InfoWorld, Vizi.Application)
 
-  G.camera.aspect = window.innerWidth / window.innerHeight;
-  G.camera.updateProjectionMatrix();
+InfoWorld.prototype.init = function(param) {
+  var cam = new Vizi.PerspectiveCamera();
+  cam.far = 100000;
+  cam.near = 1;
+  cam.fov = 45;
 
-  effect.setSize(window.innerWidth, window.innerHeight);
+  var camera = new Vizi.Object();
+  camera.addComponent(cam);
+  cam.active = true;
+  this.addObject(camera);
+
+  camera.transform.position.set(0,2,4);
+
+  var vSceneObj = new Vizi.Object();
+  var visual = new Vizi.Visual({
+    object: G.sceneObj
+  })
+  vSceneObj.addComponent(visual);
+  this.addObject(vSceneObj);
+
+  // var ground = new Vizi.Object();
+  // var visual = new Vizi.Visual({
+  //   geometry: new THREE.PlaneGeometry(2000, 2000, 100, 100),
+  //   material: new THREE.MeshBasicMaterial({
+  //     color: 0x0000ff,
+  //     transparent: true,
+  //     opacity: 1,
+  //     wireframe: true
+  //   })
+  // })
+  // ground.addComponent(visual);
+  // ground.transform.rotation.x = -Math.PI / 2
+  // this.addObject(ground)
 
 }
 
-G.animate = function() {
 
-  requestAnimationFrame(this.animate);
-  if (cameraPath !== undefined) {
-    // adjust the number after "performance.now() /" to slow down the animation speed.
-    var time = (performance.now() / 160000) % 1;
 
-    var pointA = cameraPath.getPointAt(time);
-    var pointB = cameraPath.getPointAt(Math.min(time + 0.015, 1));
+// G.scene = new THREE.Scene();
+// G.scene.fog = new THREE.Fog(0x00000, -4000, 80000);
+// G.init = function() {
+//   // this.createPrimitives();
+//   G.renderer = new THREE.WebGLRenderer({
+//     antialias: true
+//   });
+//   G.renderer.autoClear = false;
+//   document.body.appendChild(G.renderer.domElement);
 
-    pointA.z = -pointA.z;
-    pointB.z = -pointB.z;
+//   dolly = new THREE.Object3D();
+//   dolly.position.set(500, 500, 500);
+//   G.scene.add(dolly);
 
-    dolly.position.copy(pointA);
-    dolly.lookAt(pointB);
+//   G.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000000);
+//   dolly.add(G.camera);
 
-    dolly.rotateY(Math.PI); // look forward
-  }
+//   if (parameters.mode === 'cardboard') {
 
-  controls.update();
+//     controls = new THREE.DeviceOrientationControls(G.camera);
+//     effect = new THREE.StereoEffect(G.renderer);
 
-  effect.render(G.scene, G.camera);
+//   } else {
 
-}.bind(G)
-window.addEventListener('resize', G.onResize.bind(G), false);
+//     controls = new THREE.VRControls(G.camera);
+//     effect = new THREE.VREffect(G.renderer);
+
+//   }
+
+//   effect.setSize(window.innerWidth, window.innerHeight);
+
+//   document.body.addEventListener('dblclick', function() {
+
+//     effect.setFullScreen(true);
+
+//   });
+
+
+
+//   G.scene.add(G.sceneObj);
+
+
+
+// }
+// G.createPrimitives = function() {
+//   G.primitives['arc'] = new G.Arc();
+//   G.primitives['fresnal'] = new G.Fresnal();
+//   _.each(G.primitiveData, function(data, name) {
+//     if (G.primitives[name]) {
+//       G.primitives[name].init(data.numClones, data.positionData, data.sizeRange);
+//     }
+//   })
+// }
+
+// G.onResize = function() {
+
+//   G.camera.aspect = window.innerWidth / window.innerHeight;
+//   G.camera.updateProjectionMatrix();
+
+//   effect.setSize(window.innerWidth, window.innerHeight);
+
+// }
+
+// G.animate = function() {
+
+//   requestAnimationFrame(this.animate);
+//   if (cameraPath !== undefined) {
+//     // adjust the number after "performance.now() /" to slow down the animation speed.
+//     var time = (performance.now() / 160000) % 1;
+
+//     var pointA = cameraPath.getPointAt(time);
+//     var pointB = cameraPath.getPointAt(Math.min(time + 0.015, 1));
+
+//     pointA.z = -pointA.z;
+//     pointB.z = -pointB.z;
+
+//     dolly.position.copy(pointA);
+//     dolly.lookAt(pointB);
+
+//     dolly.rotateY(Math.PI); // look forward
+//   }
+
+//   controls.update();
+
+//   effect.render(G.scene, G.camera);
+
+// }.bind(G)
+// window.addEventListener('resize', G.onResize.bind(G), false);
